eb957a88ed046a9a177405240a297313c2cfa695
Rohan D'Souza <rohan.dsouza01@ad.infosys.com>
2020-04-29 23:48:02 +1200

    Users & Albums working

--- src/App.js
+++ src/App.js
@@ -8,28 +8,38 @@ class App extends Component {
     super(props);
 
     this.state = {
-      data: null,
+      data_albums: null,
+      data_users: null,
       isLoaded: false,
     };
   }
 
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/albums")
-      .then((res) => res.json())
-      .then((json) => console.log(json))
-      .then(
-        (result) => {
-          this.setState({
-            data: result.albums,
-            isLoaded: true,
-          });
-        },
-        (error) => {
-          this.setState({
-            isLoaded: false,
-          });
-        }
-      );
+  fetchData() {
+    if(!this.state.isLoaded){
+      fetch("https://jsonplaceholder.typicode.com/albums")
+        .then((res) => res.json())
+        .then((data_albums) => this.setState({ data_albums }));
+
+      fetch("https://jsonplaceholder.typicode.com/users")
+        .then((res) => res.json())
+        .then((data_users) => this.setState({ data_users }));
+     
+      this.setState({isLoaded:true})
+    
+    }
+      //.then((data) => this.setState({ data }));
+      //   (result) => {
+      //     this.setState({
+      //       data: result,
+      //       isLoaded: true,
+      //     });
+      //   },
+      //   (error) => {
+      //     this.setState({
+      //       isLoaded: false,
+      //     });
+      //   }
+      // );
 
     //this.setState({ executed: true });
 
@@ -45,6 +55,10 @@ class App extends Component {
     //console.log(this.state.data.id)
   }
 
+  fetchUsers(){
+    
+  }
+
   validator() {
     if (this.state.data != null) {
       return <p>We should have change</p>;
@@ -54,15 +68,17 @@ class App extends Component {
   handleSubmit = (e) => {
     e.preventDefault();
     console.log("hiiiiii");
-    //this.componentDidMount();
-    console.log(this.state.data);
+    this.fetchData();
+    //this.fetchUsers();
+    console.log(this.state.data_albums);
+    console.log(this.state.data_users);
   };
 
   render() {
     return (
       <div className="App">
         <Search handleSubmit={this.handleSubmit} />
-        <Albums data={this.state.data} />
+        <Albums data_albums={this.state.data_albums} data_users={this.state.data_users} />
         {this.validator()}
       </div>
     );
--- src/components/Albums.js
+++ src/components/Albums.js
@@ -2,37 +2,48 @@ import React, { Component } from "react";
 
 export class Albums extends Component {
   printAlbum() {
-    if (this.props.data!=null) {
-      this.props.data.map((flight) => {
-        if (flight.id === 1) {
-          console.log(flight.id);
-          return (
-            <React.Fragment>
-              <p>hi</p>
-              <p>id:{flight.id}</p>
-            </React.Fragment>
-          );
-        } else {
-          return (
-            <React.Fragment>
-              <p>Sup</p>
-            </React.Fragment>
-          );
-        }
+    if (this.props.data_albums != null && this.props.data_users != null) {
+      return this.props.data_albums.map((albums) => {
+        return this.props.data_users.map((users) => {
+          // if (albums.id === 1) {
+          //console.log(albums.id);
+          if (users.id === albums.userId)
+            return (
+              <React.Fragment>
+                <div
+                  style={{
+                    width: "20%",
+                    outline: "1px solid black",
+                    textAlign: "left",
+                  }}
+                >
+                  <p>User: {users.name}</p>
+                  <p>UserId: {albums.userId}</p>
+                  <p>Id: {albums.id}</p>
+                  <p>Title: {albums.title}</p>
+                </div>
+              </React.Fragment>
+            );
+          // } else {
+          //   return (
+          //     <React.Fragment>
+          //       <p>Sup</p>
+          //     </React.Fragment>
+          //   );
+          // }
+        });
       });
     } else {
       return (
         <React.Fragment>
-          <p>hi no data</p>
+          <p>Click Search To Begin</p>
         </React.Fragment>
       );
     }
   }
 
   render() {
-    return <div>
-        {this.printAlbum()}
-    </div>;
+    return <div>{this.printAlbum()}</div>;
   }
 }
 
